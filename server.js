347388a869ddae07dc67985d53b43b20084e7891
Steven Dunn <Dunn.Steven1@gmail.com>
2016-01-08 18:44:17 -0800

    Created a bare-bones Express server using Node. Created package.json and ran "npm install" to install all dependencies.

---
+++ server.js
@@ -0,0 +1,10 @@
+//A basic Webserver to serve up HTML goodness!
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
