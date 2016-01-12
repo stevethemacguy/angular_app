d0b570e3ab32e11dfc4da6f66b358707991264b2
Steven Dunn <Dunn.Steven1@gmail.com>
2016-01-11 20:29:26 -0800

    Tested different Bootstrap css files since I was having problems with Angular's progress bar components. The bars are working properly now.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 //Create a Module to be used with this app and call the module "appModule".
 //You don't have to use the same name as the variable, but it's a good idea.
 //NOTE: Some versions of phpStorm won't recognize .module or .controller...It is a bug!
-var appModule = angular.module("appModule", ['ngRoute','ui.bootstrap']);
+var appModule = angular.module("appModule", ['ngRoute','ngAnimate','ui.bootstrap']);
 
 //Configure routes for the app (i.e. "glue" the views to their respective controllers.
 appModule.config(['$routeProvider', function($routeProvider)
@@ -12,12 +12,12 @@ appModule.config(['$routeProvider', function($routeProvider)
             {
                 controller: 'SimpleController',
                 templateUrl: viewBase + 'view1.html',
-                controllerAs: 'vm'
+                /*controllerAs: 'vm'*/
             })
         .when('/view2', {
             controller: 'SimpleController',
             templateUrl: viewBase + 'view2.html',
-            controllerAs: 'vm'
+            /*controllerAs: 'vm'*/
         })
         .otherwise({ redirectTo: '/view1' });  //This could also be '/' instead
 }]);
--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -19,7 +19,45 @@ appModule.controller("SimpleController", function ($scope, simpleFactory)
                 name: $scope.newCustomer.name,
                 city: $scope.newCustomer.city
             });
-    }
+    };
+
+    $scope.max = 200;
+
+    $scope.random = function() {
+        var value = Math.floor((Math.random() * 100) + 1);
+        var type;
+
+        if (value < 25) {
+            type = 'success';
+        } else if (value < 50) {
+            type = 'info';
+        } else if (value < 75) {
+            type = 'warning';
+        } else {
+            type = 'danger';
+        }
+
+        $scope.showWarning = (type === 'danger' || type === 'warning');
+
+        $scope.dynamic = value;
+        $scope.type = type;
+    };
+    $scope.random();
+
+    $scope.randomStacked = function() {
+        $scope.stacked = [];
+        var types = ['success', 'info', 'warning', 'danger'];
+
+        for (var i = 0, n = Math.floor((Math.random() * 4) + 1); i < n; i++) {
+            var index = Math.floor((Math.random() * 4));
+            $scope.stacked.push({
+                value: Math.floor((Math.random() * 30) + 1),
+                type: types[index]
+            });
+        }
+    };
+    $scope.randomStacked();
+
 });
 
 //NOTE: This is the other way to create controllers, but I'm not sure if the simplyFactory actually works properly this way?
