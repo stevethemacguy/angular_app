84d92de5d6e1362cf3095d65a42969e0b2a12388
Steven Dunn <Dunn.Steven1@gmail.com>
2016-01-19 21:44:37 -0800

    Added angular-css, which allows you to load page-specific css files as needed. Removed view-specific includes from index.html, which are now handled when changing routes. The "main" container is now scrollable before the products load. This prevents the visible "shift" when the browser scrollbar appears on screen (after the product list extends out of the viewport).

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 //Create a Module to be used with this app and call the module "appModule".
 //You don't have to use the same name as the variable, but it's a good idea.
 //NOTE: Some versions of phpStorm won't recognize .module or .controller...It is a bug!
-var appModule = angular.module("appModule", ['ngRoute','ngAnimate','ui.bootstrap']);
+var appModule = angular.module("appModule", ['ngRoute','door3.css','ngAnimate','ui.bootstrap']);
 
 //Configure routes for the app (i.e. "glue" the views to their respective controllers.
 appModule.config(['$routeProvider', function($routeProvider)
 {
     var viewBase = 'app/views/';
+    var cssPath = 'content/css/'; //Used by routeStyles
+
     $routeProvider
         .when('/',{ redirectTo: '/home' })//just redirect to home if they come in from localhost:3000
         .when('/view1',          //This could also be '/' to default to view1
@@ -23,37 +25,43 @@ appModule.config(['$routeProvider', function($routeProvider)
         .when('/shipping', {
             title: 'Shipping',
             controller: 'ShippingController',
-            templateUrl: viewBase + 'shipping.html'
+            templateUrl: viewBase + 'shipping.html',
+            css: cssPath + 'shipping.css'
             /*controllerAs: 'vm'*/
         })
         .when('/payment', {
             title: 'Payment',
             controller: 'PaymentController',
-            templateUrl: viewBase + 'payment.html'
+            templateUrl: viewBase + 'payment.html',
+            css: cssPath + 'payment.css'
             /*controllerAs: 'vm'*/
         })
         .when('/home', {
             title: 'Home',
             controller: 'ProductController',
-            templateUrl: viewBase + 'products.html'
+            templateUrl: viewBase + 'products.html',
+            css: cssPath + 'products.css'
             /*controllerAs: 'vm'*/
         })
         .when('/confirmation', {
             title: 'Checkout Confirmation',
             controller: 'ConfirmationController',
-            templateUrl: viewBase + 'confirmation.html'
+            templateUrl: viewBase + 'confirmation.html',
+            css: cssPath + 'confirmation.css'
             /*controllerAs: 'vm'*/
         })
         .when('/cart', {
             title: 'Shopping Cart',
             controller: 'CartController',
-            templateUrl: viewBase + 'cart.html'
+            templateUrl: viewBase + 'cart.html',
+            css: [cssPath + 'products.css', cssPath + 'cart.css']
             /*controllerAs: 'vm'*/
         })
         .when('/thankyou', {
             title: 'Thank you!',
             controller: 'ThankYouController',
-            templateUrl: viewBase + 'thankyou.html'
+            templateUrl: viewBase + 'thankyou.html',
+            css: cssPath + 'thankyou.css'
             /*controllerAs: 'vm'*/
         })
         .otherwise({ redirectTo: '/home' });  //This could also be '/' instead
