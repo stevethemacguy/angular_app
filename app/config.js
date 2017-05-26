b4ee2561f302861dcd51de8c17fa15c6aa8bb195
Steven Dunn <Dunn.Steven1@gmail.com>
2017-05-25 18:22:44 -0700

    Added a new apiUrl to use with Github for continous integration on Azure. This URL points to a completely different Azure App Service and SQL database for GitHub than the one used with Visual Studio Team Services. The back-end .Net code is unchanged except for two small changes: 1. appSettings.json now has a GitHubConnectionString. 2. Startup.cs (on the back-end ProductApi project) now uses this string instead of the default VS TS url.
    To change back to the VS TS url, you will have to do the following:
    1. Change the apiUrl here in the front-end code to use the Default VS TS web server
    2. Change the Startup.cs class on the backend ProductApi project to use the default connectionString.

--- app/config.js
+++ app/config.js
@@ -1,5 +1,10 @@
 ﻿var config = {
-    apiUrl: "http://product-web-api.azurewebsites.net/api/",
+    //Default Web Server (Product API that uses Visual Studio Team Services for auto-deployments.
+    //apiUrl: "http://product-web-api.azurewebsites.net/api/",
+
+    //GitHub URL
+    apiUrl: "http://product-api-github.azurewebsites.net/api/",
+
     localHostapiUrl: "http://192.168.173.201:5000/api/",
     apiEndPoints: {}
 };
